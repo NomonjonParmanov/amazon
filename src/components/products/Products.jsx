24ae8774138e8674776c2641557c3209d4545b91
Nomonjonprog <ibragimovganijon354@gmail.com>
2024-05-30 22:58:53 +0300

    search

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const Products = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => setData(res.data.products));
+  }, []);
+
+  let product = data?.map((el) => (
+    <div key={el.id} className="card">
+      <img src={el.images[0]} alt={el.title} width={50} />
+      <h2>{el.title}</h2>
+      <h3>${el.price}</h3>
+      <p>$50</p>
+    </div>
+  ));
+  return <div className="container products">{product}</div>;
+};
+
+export default Products;
